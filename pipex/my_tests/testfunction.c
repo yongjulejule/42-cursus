8199177252bf5bffa3c98f20b7bbf7c5b7c80885
yongjulejule <lyjshow200@gmail.com>
2021-07-15 11:33:50 +0900

    BREED PROCESSES RECURSIVE: SUCCESS🎉 MATCHING suitable pipe

--- pipex/my_tests/testfunction.c
+++ pipex/my_tests/testfunction.c
@@ -1,17 +1,19 @@
-#include "../ft_pipex.h"
+#include "../includes/mandatory/ft_pipex.h"
 
 void	check_argv(char **argv)
 {
 	char *testset[30];
-
-	testset[0] = ft_strdup("/usr/bin/awk");
-	testset[1] = ft_strdup("'{printf\"%s\", $1}'");
-	testset[2] = NULL;
-	while (*argv)
-		ft_putendl_fd(*argv++, 1);
-	execve("/usr/bin/awk", testset, NULL);
+	testset[0] = ft_strdup("/usr/bin/find");
+	testset[1] = ft_strdup(".");
+	testset[2] = ft_strdup("-type");
+	testset[3] = ft_strdup("f");
+	testset[4] = ft_strdup("\( -name \"#*#\" -o name \"*~\" -o name \"#\" \)");
+	testset[5] = ft_strdup("exec rm -v {};");
+	testset[6] = NULL;
+//	while (*argv)
+//		ft_putendl_fd(*argv++, 1);
+	execve("/usr/bin/find", testset, NULL);
 }
-
 int main(int argc, char **argv)
 {
 	check_argv(argv);
--- pipex/srcs/bonus/breed_process_bonus.c
+++ pipex/srcs/bonus/breed_process_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jun <yongjule@42student.42seoul.kr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 13:40:45 by jun               #+#    #+#             */
-/*   Updated: 2021/07/14 10:28:00 by jun              ###   ########.fr       */
+/*   Updated: 2021/07/15 11:31:11 by jun              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,48 +23,34 @@ void	connect_pipe_fd(int *pipe_fd, int pipe_status)
 	close(pipe_fd[PIPE_WR]);
 }
 
-void	child_process(t_args *args, int *pipe_fd, int nth_cmd)
-{
-	if (nth_cmd == 0)
-		rdr_file_to_stdin(args->file[0]);
-	connect_pipe_fd(pipe_fd, STDOUT_FILENO);
-//ft_putstr_fd("\nin child args->params[nth_cmd][0] is : ", 2);
-//ft_putstr_fd(args->params[nth_cmd][0], 2);
-//ft_putstr_fd("\nin child nth_cmd  is ", 2);
-//ft_putnbr_fd( nth_cmd, 2);
-	execve(args->params[nth_cmd][0], args->params[nth_cmd], NULL);
-}
-
 void	parent_process(t_args *args, int *pipe_fd, int nth_cmd)
 {
 	int status;
 
-	wait(&status);
-	if (!WIFEXITED(status))
-	{
-		perror("child process quited unexpectedly");
-		exit(EXIT_FAILURE);
-	}
-//ft_putstr_fd("\nparent process nth_cmd is", 2);
-//ft_putnbr_fd(nth_cmd, 2);
-	if (nth_cmd == args->argc - 5)
+//	wait(&status);
+//	if (!WIFEXITED(status))
+//	{
+//		perror("child process quited unexpectedly");
+//		exit(EXIT_FAILURE);
+//	}
+	ft_putendl_fd("HERE IS PARENT PROCESS", 2);
+	ft_putnbr_fd(nth_cmd, 2);
+	ft_putendl_fd(args->params[nth_cmd][0],2);
+	if (args->params[nth_cmd + 1] == NULL)
 		rdr_stdout_to_file(args->file[1]);
 	connect_pipe_fd(pipe_fd, STDIN_FILENO);
-//ft_putstr_fd("\nin parent args->params[nth_cmd][0] is \n", 2);
-//ft_putstr_fd(args->params[nth_cmd + 1][0], 2);
-//ft_putnbr_fd( nth_cmd, 2);
-//ft_putstr_fd("\nin parent nth_cmd  is ", 2);
-	execve(args->params[nth_cmd + 1][0], args->params[nth_cmd + 1], NULL);
+	ft_putendl_fd("PARENT EXECVED!!", 2);
+	execve(args->params[nth_cmd][0], args->params[nth_cmd], NULL);
 }
 
-void	breed_process_recursively(t_args *args, int nth_cmd)
+void	child_process(t_args *args, int *pipe_fd, int nth_cmd)
 {
-	int		pipe_fd[2];
 	pid_t	pid;
+	int		pipe_fds[2];
 
-	if (!nth_cmd)
-		return ;
-	if (pipe(pipe_fd) == -1)
+	ft_putnbr_fd(nth_cmd, 2);
+	ft_putstr_fd(":here is child process\n", 2);
+	if (pipe(pipe_fds) == -1)
 	{
 		perror("Error while open pipe");
 		exit(EXIT_FAILURE);
@@ -72,14 +58,42 @@ void	breed_process_recursively(t_args *args, int nth_cmd)
 	pid = fork();
 	if (pid == 0)
 	{
-		breed_process_recursively(args, --nth_cmd);
-		child_process(args, pipe_fd, nth_cmd);
+		if (nth_cmd == 0)
+			rdr_file_to_stdin(args->file[0]);
+		connect_pipe_fd(pipe_fds, STDOUT_FILENO);
+		ft_putnbr_fd(nth_cmd, 2);
+		ft_putendl_fd("CHILD EXECVED!!", 2);
+		execve(args->params[nth_cmd][0], args->params[nth_cmd], NULL);
 	}
 	else if (pid > 0)
-		parent_process(args, pipe_fd, nth_cmd);
-	else
 	{
-		perror("Error while Fork");
-		exit(EXIT_FAILURE);
+		wait(NULL);
+		if (nth_cmd == args->argc - 5)
+			parent_process(args, pipe_fds, nth_cmd + 1);
+		child_process(args, pipe_fds, ++nth_cmd);
 	}
+}
+
+void	breed_process_recursively(t_args *args, int nth_cmd)
+{
+	int		pipe_fd[2];
+	pid_t	pid;
+
+	child_process(args, NULL, 0);
+//	ft_putnbr_fd(nth_cmd, 2);
+//	if (pipe(pipe_fd) == -1)
+//	{
+//		perror("Error while open pipe");
+//		exit(EXIT_FAILURE);
+//	}
+//	pid = fork();
+//	if (pid == 0)
+//		child_process(args, pipe_fd, nth_cmd - 2);
+//	else if (pid > 0)
+//		parent_process(args, pipe_fd, nth_cmd);
+//	else
+//	{
+//		perror("Error while Fork");
+//		exit(EXIT_FAILURE);
+//	}
 }
