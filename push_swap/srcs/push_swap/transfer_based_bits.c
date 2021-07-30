6c448e68ad6a5f877e2c4b1d93bef84a649b37fb
yongjulejule <lyjshow200@gmail.com>
2021-07-30 21:51:55 +0900

    LSD and MSD compare,,, LSD looks more promising🤣

--- push_swap/srcs/push_swap/transfer_based_bits.c
+++ push_swap/srcs/push_swap/transfer_based_bits.c
@@ -6,7 +6,7 @@
 /*   By: jun <yongjule@42student.42seoul.kr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 10:51:11 by jun               #+#    #+#             */
-/*   Updated: 2021/07/30 17:20:10 by jun              ###   ########.fr       */
+/*   Updated: 2021/07/30 20:32:43 by jun              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	transf_a_to_b_0(t_stk **a, t_stk **b, t_stk **op_a, t_stk **op_b)
 	{
 		do_op(a, b, op_a, PB);
 		(*b)->ac++;
-		if (!(((*b)->head->next->idx >> (bit_shift - 1)) & 1))
+		if ((((*b)->head->next->idx >> (bit_shift + 1)) & 1))
 		{
 			do_op(NULL, b, op_a, RB);
 			cnt++;
@@ -45,7 +45,7 @@ int	transf_a_to_b_1(t_stk **a, t_stk **b, t_stk **op_a, t_stk **op_b)
 	{
 		do_op(a, b, op_a, PB);
 		(*b)->ac++;
-		if ((((*b)->head->next->idx >> (bit_shift - 1)) & 1))
+		if ((((*b)->head->next->idx >> (bit_shift + 1)) & 1))
 		{
 			do_op(NULL, b, op_a, RB);
 			cnt++;
@@ -63,8 +63,8 @@ static void init_transf(t_stk **a, t_stk **b, t_stk **op_a, t_stk **op_b)
 	int		nbr;
 
 	bit_shift = (*op_a)->ac;
-	(*op_a)->ac = bit_shift - 1;
-	while ((*op_a)->ac > 5)
+	(*op_a)->ac = 0;
+	while ((*op_a)->ac < 2)
 	{
 		cnt = 0;
 		nbr = (*b)->ac;
@@ -89,99 +89,100 @@ static void init_transf(t_stk **a, t_stk **b, t_stk **op_a, t_stk **op_b)
 //			do_op(NULL, b, op_a, RRB);
 //			cnt--;
 //		}
-		(*op_a)->ac--;
+		(*op_a)->ac++;
 	}
 	(*a)->ac =(*a)->ac - (*b)->ac;
 	print_stack(*a, *b, NULL);
 
-	while ((*op_a)->ac > 3)
-	{
-		cnt = 0;
-		nbr = (*a)->ac;
-		while (nbr < (*b)->ac)
-		{
-			cnt += transf_a_to_b_0(b, a, op_a, op_b);
-			nbr++;
-		}
-//		while (cnt > 0)
+//	while ((*op_a)->ac < 4)
+//	{
+//		cnt = 0;
+//		nbr = (*a)->ac;
+//		while (nbr < (*b)->ac)
 //		{
-//			do_op(NULL, a, op_a, RRB);
-//			cnt--;
+//			cnt += transf_a_to_b_0(b, a, op_a, op_b);
+//			nbr++;
 //		}
-		nbr = (*a)->ac;
-		while (nbr < (*b)->ac)
-		{
-			cnt += transf_a_to_b_1(b, a, op_a, op_b);
-			nbr++;
-		}
-//		while (cnt > 0)
+////		while (cnt > 0)
+////		{
+////			do_op(NULL, a, op_a, RRB);
+////			cnt--;
+////		}
+//		nbr = (*a)->ac;
+//		while (nbr < (*b)->ac)
 //		{
-//			do_op(NULL, a, op_a, RRB);
-//			cnt--;
+//			cnt += transf_a_to_b_1(b, a, op_a, op_b);
+//			nbr++;
 //		}
-		(*op_a)->ac--;
-	}
-	(*b)->ac =(*b)->ac - (*a)->ac;
-	print_stack(*a, *b, NULL);
-
-	while ((*op_a)->ac > 1)
-	{
-		cnt = 0;
-		nbr = (*b)->ac;
-		while (nbr < (*a)->ac)
-		{
-			cnt += transf_a_to_b_0(a, b, op_a, op_b);
-			nbr++;
-		}
+////		while (cnt > 0)
+////		{
+////			do_op(NULL, a, op_a, RRB);
+////			cnt--;
+////		}
+//		(*op_a)->ac++;
+//	}
+//	(*b)->ac =(*b)->ac - (*a)->ac;
+//	print_stack(*a, *b, NULL);
+//
+//	while ((*op_a)->ac < 6)
+//	{
+//		cnt = 0;
+//		nbr = (*b)->ac;
+//		while (nbr < (*a)->ac)
+//		{
+//			cnt += transf_a_to_b_0(a, b, op_a, op_b);
+//			nbr++;
+//		}
 //		while (cnt > 0)
 //		{
 //			do_op(NULL, b, op_a, RRB);
 //			cnt--;
 //		}
-		nbr = (*b)->ac;
-		while (nbr < (*a)->ac)
-		{
-			cnt += transf_a_to_b_1(a, b, op_a, op_b);
-			nbr++;
-		}
+//		nbr = (*b)->ac;
+//		while (nbr < (*a)->ac)
+//		{
+//			cnt += transf_a_to_b_1(a, b, op_a, op_b);
+//			nbr++;
+//		}
 //		while (cnt > 0)
 //		{
 //			do_op(NULL, b, op_a, RRB);
 //			cnt--;
 //		}
-		(*op_a)->ac--;
-	}
-	(*a)->ac =(*a)->ac - (*b)->ac;
-	print_stack(*a, *b, NULL);
+//		(*op_a)->ac++;
+//	}
+//	(*a)->ac =(*a)->ac - (*b)->ac;
+//	print_stack(*a, *b, NULL);
+//
+//	while ((*op_a)->ac < 8)
+//	{
+//		cnt = 0;
+//		nbr = (*a)->ac;
+//		while (nbr < (*b)->ac)
+//		{
+//			cnt += transf_a_to_b_0(b, a, op_a, op_b);
+//			nbr++;
+//		}
+//		while (cnt > 0)
+//		{
+//			do_op(NULL, a, op_a, RRB);
+//			cnt--;
+//		}
+//		nbr = (*a)->ac;
+//		while (nbr < (*b)->ac)
+//		{
+//			cnt += transf_a_to_b_1(b, a, op_a, op_b);
+//			nbr++;
+//		}
+//		while (cnt > 0)
+//		{
+//			do_op(NULL, a, op_a, RRB);
+//			cnt--;
+//		}
+//		(*op_a)->ac++;
+//	}
+//	(*b)->ac =(*b)->ac - (*a)->ac;
 
-	while ((*op_a)->ac > 0)
-	{
-		cnt = 0;
-		nbr = (*a)->ac;
-		while (nbr < (*b)->ac)
-		{
-			cnt += transf_a_to_b_0(b, a, op_a, op_b);
-			nbr++;
-		}
-//		while (cnt > 0)
-//		{
-//			do_op(NULL, a, op_a, RRB);
-//			cnt--;
-//		}
-		nbr = (*a)->ac;
-		while (nbr < (*b)->ac)
-		{
-			cnt += transf_a_to_b_1(b, a, op_a, op_b);
-			nbr++;
-		}
-//		while (cnt > 0)
-//		{
-//			do_op(NULL, a, op_a, RRB);
-//			cnt--;
-//		}
-		(*op_a)->ac--;
-	}
-	(*b)->ac =(*b)->ac - (*a)->ac;
 	print_stack(*a, *b, NULL);
 	(*op_a)->ac = bit_shift;
 	print_stack(*op_a, *op_b, NULL);
